28f076eb619e384b816f9e869885ad7611c8ff04
Revazashvili <levanrevazashvili@gmail.com>
2021-12-09 12:52:06 +0400

    add ContextCreator.cs

---
+++ src/Constructor.Core/Creators/ContextCreator.cs
@@ -0,0 +1,24 @@
+using Constructor.Core.Constructors;
+using Constructor.Core.Managers;
+
+namespace Constructor.Core.Creators
+{
+    public class ContextCreator : IContextCreator
+    {
+        private readonly IContextConstructor _contextConstructor;
+        private readonly IFileManager _fileManager;
+
+        public ContextCreator(IContextConstructor contextConstructor,IFileManager fileManager)
+        {
+            _contextConstructor = contextConstructor;
+            _fileManager = fileManager;
+        }
+
+        public void Create(CreateContextOptions createContextOptions)
+        {
+            var construction = _contextConstructor.Constructor(createContextOptions.ContextOptions);
+            var pathToFile = $@"{createContextOptions.Path}\{createContextOptions.ContextOptions.Name}.cs";
+            _fileManager.Save(pathToFile, construction);
+        }
+    }
+}
---
+++ src/Constructor.Core/Creators/CreateContextOptions.cs
@@ -0,0 +1,16 @@
+using Constructor.Core.Models;
+
+namespace Constructor.Core.Creators
+{
+    public class CreateContextOptions
+    {
+        public CreateContextOptions(ContextOptions contextOptions, string path)
+        {
+            ContextOptions = contextOptions;
+            Path = path;
+        }
+
+        public ContextOptions ContextOptions { get; set; }
+        public string Path { get; set; }
+    }
+}
