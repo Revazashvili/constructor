434eb3b4efe7f9fc611a8b547ba0cdda5adb7f12
Revazashvili <levanrevazashvili@gmail.com>
2021-12-09 13:39:08 +0400

    CibContext.cs

--- src/TestClient/CibContext.cs
+++ src/TestClient/CibContext.cs
@@ -1,10 +1,19 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using TestClient.Entities;
 
 namespace TestClient
 {
     public class CibContext : DbContext
     {
+        public CibContext(DbContextOptions<CibContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerResponse> CustomerResponses { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
